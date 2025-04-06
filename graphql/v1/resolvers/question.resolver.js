322d5f6b2406beffdd564429fa430d73cdbf500d
Mark Yun <only.hyeok@gmail.com>
2025-04-06 11:21:41 +0900

    Update import paths and enhance GraphQL types for Question and Topic models

--- graphql/v1/resolvers/question.resolver.js
+++ graphql/v1/resolvers/question.resolver.js
@@ -1,5 +1,5 @@
 // models/graphql/QuestionResolvers.js
-import Question from '@models/Question.js';
+import Question from '../../../models/Question.js';
 
 export const resolvers = {
     Query: {
--- graphql/v1/resolvers/topic.resolver.js
+++ graphql/v1/resolvers/topic.resolver.js
@@ -1,5 +1,5 @@
 // models/graphql/TopicResolvers.js
-import Topic from '@models/Topic.js';
+import Topic from '../../../models/Topic.js';
 
 export const resolvers = {
     Query: {
